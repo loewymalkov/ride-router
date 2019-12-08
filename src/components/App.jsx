198e990fc6d139b6cdddb57f37ed12b79119407b
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-12-08 10:01:34 -0800

    add code to App and restructure routes

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import RouteList from './RouteList';
+import NewRoute from './NewRoute';
+import Login from './Login';
+import Header from './Header';
+import { Switch, Route } from 'react-router-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterRouteList: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/routes" component={RouteList} />
+          <Route exact path="/newroute" component={NewRoute} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
