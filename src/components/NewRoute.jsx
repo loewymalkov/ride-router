e659a0f0f7f9b854da6776bf2458ed3e60ec49aa
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-12-08 10:25:16 -0800

    add NewRoute form component, proptypes

--- src/components/NewRoute.jsx
+++ src/components/NewRoute.jsx
@@ -1,12 +1,32 @@
-return (
-  <div>
-    <h1>Upload a new route</h1>
-    <form onSubmit={handleFormSubmission}>
-    <textarea
-      id='routeInfo'
-      placeholder='Upload your map'
-      ref={(textarea) => { _routeInfo = textarea; }} />
-      <button type='submit'>Upload</button>
-    </form>
-  </div>
-)
+import React from 'react';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+
+function NewRoute(props) {
+  let _routeInfo = null;
+
+  function handleRouteSubmission(event) {
+    event.preventDefault();
+    props.onNewRouteCreation({routeInfo: _routeInfo, id: v4()});
+    _routeInfo.value = '';
+  }
+
+  return (
+    <div>
+      <h1>Upload a new route</h1>
+      <form onSubmit={handleRouteSubmission}>
+      <textarea
+        id='routeInfo'
+        placeholder='Upload your map'
+        ref={(textarea) => { _routeInfo = textarea; }} />
+        <button type='submit'>Upload</button>
+      </form>
+    </div>
+  );
+}
+
+NewRoute.propTypes = {
+  onNewRouteCreation: PropTypes.func
+};
+
+export default NewRoute;
