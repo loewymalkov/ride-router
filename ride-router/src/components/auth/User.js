f7c2ef0625ba17839f7767e7b8dd60b505aca198
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-12-18 15:47:15 -0800

    add moment data to user account and route details

--- ride-router/src/components/auth/User.js
+++ ride-router/src/components/auth/User.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import moment from 'moment';
 
 const User = (props) => {
   const { auth } = props;
@@ -9,7 +10,8 @@ const User = (props) => {
     <div className="container center">
       <ul>
         <li>Username: {props.profile.firstName} </li>
-        <li>Date of account creation:  { Date() }</li>
+        <li>Email: {props.auth.email} </li>
+        <li>Sign up date:  {moment.unix(props.auth.createdAt / 1000).fromNow()}</li>
       </ul>
     </div>
   );
--- ride-router/src/components/routes/RouteDetails.js
+++ ride-router/src/components/routes/RouteDetails.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 import { Redirect } from 'react-router-dom';
+import moment from 'moment';
 
 const RouteDetails = (props) => {
   const { route, auth } = props;
@@ -16,7 +17,7 @@ const RouteDetails = (props) => {
             <span className="card-title">{ route.title } </span>
             <embed src={ route.routeInfo } width="600" height="600" title="map"></embed>
             <p>{route.userFirstName} {route.userLastName}</p>
-            <p> {route.createdAt.toDate().toString()}</p>
+            <p> {moment(route.createdAt.toDate()).calendar()}</p>
           </div>
         </div>
       </div>
