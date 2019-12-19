0e011a0541399886f102f83c7e9c501e6bc7366e
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-12-18 21:37:58 -0800

    update user account info

--- ride-router/src/components/auth/User.js
+++ ride-router/src/components/auth/User.js
@@ -7,17 +7,17 @@ const User = (props) => {
   const { auth } = props;
   if (!auth.uid) return <Redirect to ='/sign' />
   return(
-    <div className="container center">
+    <div className="container center" style={{marginTop: 100}}>
       <div className="card" style={{background: 'rgba(255, 255, 255, 0.6)', padding: 5}}>
         <div className="card-title grey-text text-darken-3">Account Info</div>
-        <ul className="grey-text text-darken-3" style={{margin: 40}}>
+        <ul className="grey-text text-darken-3" style={{margin: 30}}>
            
           <li style={{margin: 20}}>Name: {props.profile.firstName} {props.profile.lastName} </li>
 
           <li style={{margin: 20}} >Email: {props.auth.email} </li>
 
           <li style={{margin: 20}} >Sign up date:  {moment.unix(props.auth.createdAt / 1000).fromNow()}</li>
-          
+
         </ul>
       </div>
     </div>
